7f94b2435c97afde6db8a4d89aba97c4cde60eef
JavaNi80 <71756562+Tebing27@users.noreply.github.com>
2026-04-28 19:52:39 +0700

    deploy uts

---
+++ app/src/main/java/com/example/uts_prak_mobile/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.uts_prak_mobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnWisata = findViewById<Button>(R.id.btn_wisata)
+        btnWisata.setOnClickListener {
+            val intent = Intent(this, Wisata::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/uts_prak_mobile/Wisata.kt
@@ -0,0 +1,25 @@
+package com.example.uts_prak_mobile
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Wisata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_wisata)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnMain = findViewById<Button>(R.id.btn_main)
+        btnMain.setOnClickListener {
+            finish()
+        }
+    }
+}
